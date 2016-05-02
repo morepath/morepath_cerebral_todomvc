7989eaca2a79d5cab048725e2a51b419d1ed21d0
Henri Hulski <henri.hulski@gazeta.pl>
2016-05-02 16:36:46 +0200

    Preparing release 0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ description = (
 long_description = (
     io.open('README.rst', encoding='utf-8').read() + '\n\n' +
     io.open('CHANGES.rst', encoding='utf-8').read())
-version = '0.1.0'
+version = '0.1'
 
 setup(
     name=name,
